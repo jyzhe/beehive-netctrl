ff2ab66b9ad603eb68dc9d1eff6647674248616e
KENAN DENG <dknyxh@gmail.com>
2016-03-23 14:51:34 -0400

    add mac and testing eviroment

---
+++ routing/router_mac.go
@@ -0,0 +1,294 @@
+package routing
+
+import (
+
+    "fmt"
+
+    bh "github.com/kandoo/beehive"
+    "github.com/kandoo/beehive-netctrl/nom"
+    "github.com/jyzhe/beehive-netctrl/discovery"
+)
+
+// Router is the main handler of the routing application.
+type RouterMAC struct{
+    // switching.Hub
+    discovery.GraphBuilderCentralized
+}
+const (
+    areaTonode = 'areaTonode' // AreaID mapping to a node map!, areaID -> map[node's mac] port UID
+    borderNode = 'bordernode' // AreaID mapping to a list of node!, areaID -> map list of node
+
+
+)
+type areaQuery{ // Area query, only give the master conroller this  areaid -> another areaid
+    from string
+    to string
+}
+
+type setupArea{
+    area string
+}
+
+func FindAreaId(ID nom.NodeID) string{ // This should take a node id, then return an area id. like 1 -> 1, a -> 2. return as string. Consider hardcoded.
+    if ID == '1' || ID == '2' || ID == '3' || ID == '4' || ID == '5'{
+        return '1'
+    }
+    else{
+        return '2'
+    }
+}
+func FindAreaIdMac(m nom.MACAddr)string{
+    a1 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
+    a2 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x02}
+    a3 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x03}
+    a4 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x04}
+    a5 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x05}
+    if m == a1 || m == a2 || m == a3 || m == a4 || m == a5{
+        return '1'
+    }
+    else{
+        return '2'
+    }
+}
+
+func (r Router) Rcv(msg bh.Msg, ctx bh.RcvContext) error {
+
+    switch msg.Data().(type) {
+    case areaQuery:
+        fmt.Println("HERE")
+        return nil
+    case setupArea:
+        return registerEndhostsMAC(ctx,)
+    case nom.LinkAdded:
+        return r.GraphBuilderCentralized.Rcv(msg, ctx)
+    case nom.LinkDeleted:
+        return r.GraphBuilderCentralized.Rcv(msg, ctx)
+    default:
+        in := msg.Data().(nom.PacketIn)
+        src := in.Packet.SrcMAC()
+        dst := in.Packet.DstMAC()
+
+        areaTonodeDic := ctx.Dict(areaTonode)
+        d := areaTonode.Get(in.Node)
+        if dst.IsLLDP() {
+            return nil
+        }
+
+        // FIXME: Hardcoding the hardware address at the moment
+        srck := src.Key()
+        _, src_err := d.(map[string]nom.UID)[srck]
+        if src_err != true {
+            fmt.Printf("Router: Error retrieving hosts %v\n", src)
+            return nil
+        }
+
+        if dst.IsBroadcast() || dst.IsMulticast() {
+            fmt.Printf("Router: Received Broadcast or Multicast from %v\n", src)
+            return nil
+        }
+
+        sn := in.Node
+
+        dstk := dst.Key()
+        dst_port, dst_err := d.(map[string]nom.UID)[dstk]
+        if  dst_err != true {
+            fmt.Printf("Router: Cant find dest node %v\n", dstk)
+            ctx.Emit(areaQuery{})
+            return nil
+        }
+        dn,_ := nom.ParsePortUID(dst_port.(nom.UID))
+        p := dst_port.(nom.UID)
+
+        if (sn != nom.UID(dn)){
+
+            paths, shortest_len := discovery.ShortestPathCentralized(sn, nom.UID(dn), ctx)
+            fmt.Printf("Router: Path between %v and %v returns %v, %v\n", sn, nom.UID(dn), paths, shortest_len)
+
+            for _, path := range paths {
+                if len(path) != shortest_len {
+                    continue
+                } else {
+
+                    p = path[0].From
+                    break
+                }
+            }
+        }
+
+        if src_err == nil {
+
+            // Forward flow entry
+            add_forward := nom.AddFlowEntry{
+                Flow: nom.FlowEntry{
+                    Node: in.Node,
+                    Match: nom.Match{
+                        Fields: []nom.Field{
+                            nom.EthDst{
+                                Addr: dst,
+                                Mask: nom.MaskNoneMAC,
+                            },
+                        },
+                    },
+                    Actions: []nom.Action{
+                        nom.ActionForward{
+                            Ports: []nom.UID{p},
+                        },
+                    },
+                },
+            }
+            ctx.Reply(msg, add_forward)
+
+        }
+
+        if dst_err == nil {
+
+            // Reverse flow entry
+            add_reverse := nom.AddFlowEntry{
+                Flow: nom.FlowEntry{
+                    Node: in.Node,
+                    Match: nom.Match{
+                        Fields: []nom.Field{
+                            nom.EthDst{
+                                Addr: src,
+                                Mask: nom.MaskNoneMAC,
+                            },
+                        },
+                    },
+                    Actions: []nom.Action{
+                        nom.ActionForward{
+                            Ports: []nom.UID{in.InPort},
+                        },
+                    },
+                },
+            }
+            ctx.Reply(msg, add_reverse)
+
+        }
+
+        out := nom.PacketOut{
+            Node:     in.Node,
+            InPort:   in.InPort,
+            BufferID: in.BufferID,
+            Packet:   in.Packet,
+            Actions: []nom.Action{
+                nom.ActionForward{
+                    Ports: []nom.UID{p},
+                },
+            },
+        }
+        ctx.Reply(msg, out)
+    }
+
+    return nil
+
+}
+
+// Rcv maps Discovery based on its destination node and Advertisement messages
+// based on their source node.
+func (r Router) Map(msg bh.Msg, ctx bh.MapContext) bh.MappedCells {
+    switch msg.Data().(type){
+    case setupArea:
+        set := msg.Data().(setupArea)
+        return bh.MappedCells{{areaTonode, set.area}}
+    case nom.PacketIn:
+        packetin := msg.Data().(nom.PacketIn)
+        ni, _ := nom.ParsePortUID(packetin.Node)
+        return bh.MappedCells{{areaTonode, FindAreaId(ni)}}
+    case nom.LinkAdded:
+        var link = nom.Link
+        link = nom.Link(dm)
+        nf,_ := nom.ParsePortUID(link.From)
+        nt,_ := nom.ParsePortUID(link.From)
+        if FindAreaId(nf)!= FindAreaId(nt){
+            return bh.MappedCells{{"__D__", "__0__"}}  
+        }
+        else{
+            return bh.MappedCells{{areaTonode, FindAreaId(nf)}}
+        }
+    case nom.LinkDeleted:
+        var link = nom.Link
+        link = nom.Link(dm)
+        nf,_ := nom.ParsePortUID(link.From)
+        nt,_ := nom.ParsePortUID(link.From)
+        if FindAreaId(nf)!= FindAreaId(nt){
+            return bh.MappedCells{{"__D__", "__0__"}}  
+        }
+        else{
+            return bh.MappedCells{{areaTonode, FindAreaId(nf)}}
+        }
+    case areaQuery
+        return bh.MappedCells{{"__D__", "__0__"}}
+    }
+
+    return bh.MappedCells{{"__D__", "__0__"}}
+    //"__D__" , "__0__" is the master controller
+    //case nom.LinkAdded: {__D__, __0__} , {areaTonode, }
+
+    // switch dm := msg.Data().(type) {
+    // case nom.LinkAdded:
+    //  from := dm.From
+    //  n, _ := nom.ParsePortUID(from)
+    //  return bh.MappedCells{{"N", string(n)}}
+    // case nom.LinkDeleted:
+    //  from := dm.From
+    //  n, _ := nom.ParsePortUID(from)
+    //  return bh.MappedCells{{"N", string(n)}}
+    // default:
+    //  return bh.MappedCells{{"N", string(msg.Data().(nom.PacketIn).Node)}}
+    // }
+}
+
+func InstallRouterMAC(h bh.Hive, opts ...bh.AppOption) {
+
+    app := h.NewApp("Router", opts...)
+    router := RouterMAC{}
+
+    // handle routing of packets
+    app.Handle(nom.PacketIn{}, router)
+    // building centralized network topology
+    app.Handle(nom.LinkAdded{}, router)
+    app.Handle(nom.LinkDeleted{}, router)
+    app.Handle(areaQuery{}, router)
+
+    app.Handle(setupArea{}, router)
+    h.Emit(setupArea{'1'})
+    // h.Emit(setupArea{'2'})
+    // app.Handle(discovery.RegisterBorderNode{}, router)
+
+    fmt.Println("Installing Router....")
+}
+
+func registerEndhostsMAC(ctx bh.RcvContext, area string){
+    gob.Register(map[string]nom.UID{})
+    d := ctx.Dict(areaTonode)
+    if area == '1'{
+        m := make(map[string]nom.UID)
+        a1 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
+        m[nom.MACAddr(a1).Key()] = nom.UID("4$$1")
+        a2 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x02}
+        m[nom.MACAddr(a2).Key()] = nom.UID("4$$2")
+        a3 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x03}
+        m[nom.MACAddr(a3).Key()] = nom.UID("5$$1")
+        a4 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x04}
+        m[nom.MACAddr(a4).Key()] = nom.UID("5$$2")
+        d.Put(string(1),m)
+    }
+    else if area == '2'{
+        m := make(map[string]nom.UID)
+        a5 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x05}
+        m[nom.MACAddr(a5).Key()] = nom.UID("9$$1")
+        a6 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x06}
+        m[nom.MACAddr(a6).Key()] = nom.UID("9$$2")
+        a7 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x07}
+        m[nom.MACAddr(a7).Key()] = nom.UID("a$$1")
+        a8 := [6]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x08}
+        m[nom.MACAddr(a8).Key()] = nom.UID("a$$2") 
+        d.Put(string(2),m)
+    }
+
+
+    return nil
+
+}
+
+}
---
+++ routing/topologies/fattree-kenan.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+
+"""
+BeeHive - Load Balancing and Routing
+Author: Jay Yu
+February 5th, 2016
+
+Start up a fat tree topology with multiple paths among hosts. The tree has a
+3-layer hierarchical structure.
+"""
+
+import sys
+from time import sleep, time
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.log import setLogLevel
+from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.cli import CLI
+from mininet.util import pmonitor
+from signal import SIGINT
+
+class ThreeLayerTopo(Topo):
+
+    def __init__(self, k=2, **opts):
+
+        Topo.__init__(self, **opts)
+
+        self.k = k
+        switchCount = 1
+
+        hosts   = []
+        aggr    = []
+        tor     = []
+        core    = []
+        # Create the pods
+        for i in range(k):
+
+            core.append(self.addSwitch('s%s' % switchCount))
+            switchCount += 1
+
+            # Adding aggregation switches
+            for j in range(2):
+                aggr.append(self.addSwitch('s%d'%(switchCount)))
+                switchCount += 1
+
+            # Adding ToR switches
+            for j in range(2):
+                tor.append(self.addSwitch('s%d'%(switchCount)))
+                switchCount += 1
+
+            # Adding hosts
+            for j in range(4):
+                hosts.append(self.addHost('h%d'%(i * 4 + j)))
+
+        # Adding the links
+        for i in range(k):
+
+            # Connect hosts to tor
+            for j in range(4):
+                self.addLink(hosts[i * 4 + j], tor[i * 2 + j // 2])
+
+            # Connect tor to aggr
+            self.addLink(aggr[i * 2], tor[i * 2])
+            self.addLink(aggr[i * 2 + 1], tor[i * 2])
+            self.addLink(aggr[i * 2], tor[i * 2 + 1])
+            self.addLink(aggr[i * 2 + 1], tor[i * 2 + 1])
+
+        # Connect pods to core
+        for i in range(k * 2):
+
+            idx = 0 if i % 2 == 0 else k / 2
+            for j in range(k / 2):
+                self.addLink(core[j + idx], aggr[i])
+
+topos = { 'mytopo': ( lambda: ThreeLayerTopo() ) }
+
+class MultiSwitch(OVSSwitch):
+
+    map = {}
+
+    def init_map(self, controllers):
+        for i in range(2):
+            for j in range(1, 6):
+                self.map["s%d"%(i * 5 + j)] = controllers[i]
+
+
+    def start(self, controllers):
+        self.init_map(controllers)
+        return OVSSwitch.start(self, [self.map[self.name]])
+
+def start_mininet():
+
+    fattree = ThreeLayerTopo()
+
+    c0 = RemoteController(name="beehive-netctrl", ip="127.0.0.1", port=6633)
+    c1 = RemoteController(name="beehive-netctrl2", ip="127.0.0.1", port=6634)
+    # c2 = RemoteController(name="beehive-master", ip="127.0.0.1", port=9080)
+
+    net = Mininet(topo=fattree, switch=MultiSwitch, build=False,
+                  autoSetMacs=True, autoStaticArp=True)
+
+
+    for c in [c0, c1]:
+        net.addController(c)
+
+    # net.addController(c2)
+
+    net.build()
+    net.start()
+
+    # print "Performing prelimilary testing...."
+    # pre_test(net)
+
+    CLI(net)
+    net.stop()
+
+def pre_test(net):
+
+    print "Waiting for the controller to complete handshake..."
+    sleep(5)
+
+    popens = {}
+    hosts = net.hosts
+
+    print "Flow 1: h0 starting to ping h6..."
+    popens[hosts[0]] = hosts[0].popen('ping', hosts[6].IP())
+    sleep(1)
+    print "Flow 2: h1 starting to ping h7..."
+    popens[hosts[1]] = hosts[1].popen('ping', hosts[7].IP())
+
+    endTime = time() + 10
+    for h, line in pmonitor( popens, timeoutms=500 ):
+        if h:
+           print 'Flow %d: %s' % ( hosts.index(h) + 1, line ),
+        if time() >= endTime:
+           for p in popens.values():
+             p.send_signal( SIGINT )
+
+if __name__ == "__main__":
+
+    setLogLevel('info')
+
+    start_mininet()
