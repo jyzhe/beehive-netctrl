7d61c02cf621a119d41cc2c6e60982b3083c7076
KENAN DENG <dknyxh@gmail.com>
2016-04-04 16:17:56 -0400

    Add bash command

---
+++ routing/run_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $GOPATH/src/github.com/jyzhe/beehive-netctrl/
+rm -rf /tmp/beehive*
+go run main-kenan.go -of.addr 0.0.0.0:9088 &
+go run main2-kenan.go -addr localhost:7678 -statepath /tmp/beehive2 -paddrs localhost:7677 &
+go run main3-kenan.go -addr localhost:7679 -of.addr 0.0.0.0:6634 -statepath /tmp/beehive3 -paddrs localhost:7677 &
+python routing/topologies/fattree.py
