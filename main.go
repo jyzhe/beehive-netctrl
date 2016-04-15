1aac04100a070743e53b95337655c0ae1dc70a87
Zhe Yu <jay.yzhe@gmail.com>
2016-04-15 09:05:04 -0700

    using parameters

--- main.go
+++ main.go
@@ -1,50 +1,51 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"runtime/pprof"
-	"time"
+    "flag"
+    "log"
+    "os"
+    "runtime/pprof"
 
-	bh "github.com/kandoo/beehive"
-	"github.com/kandoo/beehive-netctrl/controller"
-	"github.com/jyzhe/beehive-netctrl/discovery"
-	"github.com/kandoo/beehive-netctrl/openflow"
-	"github.com/jyzhe/beehive-netctrl/routing"
-	// "github.com/jyzhe/beehive-netctrl/switching"
+    bh "github.com/kandoo/beehive"
+    "github.com/kandoo/beehive-netctrl/controller"
+    "github.com/jyzhe/beehive-netctrl/discovery"
+    "github.com/kandoo/beehive-netctrl/openflow"
+    "github.com/jyzhe/beehive-netctrl/routing"
+    // "github.com/jyzhe/beehive-netctrl/switching"
 )
 
-var from = flag.Int("from", 1, "First pod in [1..k]")
-var to = flag.Int("to", 4, "Last pod in [1..k]")
-var k = flag.Int("k", 4, "Number of ports of switches (must be even)")
-var epoc = flag.Duration("epoc", 100*time.Millisecond,
-	"The duration between route advertisement epocs.")
+var controllertype = flag.String("controller", "master", "specify the controller (master, area1, area2)")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
+    flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
+
+    h := bh.NewHive()
+    openflow.StartOpenFlow(h)
+    controller.RegisterNOMController(h)
+    discovery.RegisterDiscovery(h)
+
+    // Register a switch:
+    // switching.RegisterSwitch(h, bh.Persistent(1))
+    // or a hub:
+    // switching.RegisterHub(h, bh.NonTransactional())
+	if *controllertype == "master" {
+		routing.InstallMasterC(h, bh.Persistent(1))
+	} else if *controllertype == "area1" {
+		routing.InstallR1(h, bh.Persistent(1))
+	} else {
+		routing.InstallR2(h, bh.Persistent(1))
 	}
 
-	h := bh.NewHive()
-	openflow.StartOpenFlow(h)
-	controller.RegisterNOMController(h)
-	discovery.RegisterDiscovery(h)
-
-	// Register a switch:
-	// switching.RegisterSwitch(h, bh.Persistent(1))
-	// or a hub:
-	// switching.RegisterHub(h, bh.NonTransactional())
-
-	routing.InstallRouter(h, bh.Persistent(1))
-	// routing.InstallLoadBalancer(h, bh.Persistent(1))
-	// routing.InstallRouterIP(h,  bh.Persistent(1))
-	h.Start()
+    // routing.InstallLoadBalancer(h, bh.Persistent(1))
+    // routing.InstallRouterIP(h,  bh.Persistent(1))
+    h.Start()
 }
--- readme.txt
+++ readme.txt
@@ -2,16 +2,13 @@ To clear the previous instances of beehive and their states
     rm -rf /tmp/beehive*
 
 To run master controller:
-    go run master.go -of.addr 0.0.0.0:9080
+    go run main.go -of.addr 0.0.0.0:9080
 
 In a separate window, run the first controller for area 1:
-    go run router.go -addr localhost:7678 -statepath /tmp/beehive2 -paddrs localhost:7677
+    go run main.go -addr localhost:7678 -statepath /tmp/beehive2 -paddrs localhost:7677 -controller area1
 
 In a separate window, runt he second controller for area 2:
-    go run loadbalancer.go -addr localhost:7679 -of.addr 0.0.0.0:6634 -statepath /tmp/beehive3 -paddrs localhost:7677
+    go run main.go -addr localhost:7679 -of.addr 0.0.0.0:6634 -statepath /tmp/beehive3 -paddrs localhost:7677 -controller area2
 
 Then start Mininet (in the topologies directory):
     sudo python fattree.py
-
-You may also run controllers in background using &, for example;
-    go run master.go -of.addr 0.0.0.0:9080 &
